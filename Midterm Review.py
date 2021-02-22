899b79b4ce60c2a378726fcb5f3ae2a79b50852a
ritalaplante <45489428+ritalaplante@users.noreply.github.com>
2021-02-22 14:55:59 -0500

    Add files via upload

---
+++ Midterm Review.py
@@ -0,0 +1,25 @@
+#Nested if else statements
+#1. Write a funtion that takes in someone's age if their age is a multiple
+#of 5 tell them they win a free vacation. If their age is also a multiple of 5
+#tell them they win a car. If their age is also an even number tell them they win
+#a house
+#2. write in a function that takes in a number and tells the user if the number
+#is a positive single-digit number. If it is a positive single digit number, tell
+#the user that it is a positive single digit number. Otherwise, tell the user it
+#not a positive single digit number. 
+
+#And Or
+#Write a function that does the same thing as the functions described above
+#but uses and or statements instead of nested if else statements
+
+#for loops
+#1. Write a function called count down which takes in an integer and counts down by
+#3s until you get to zero. Print the index for each iteration of countdown and
+#let the user know if that index is even or odd.
+#2. Write a function that takes in a string and uses a for loop to reverse that
+#string.
+
+#While loops
+#1. Write the reverse string function using a while loop
+#Write a function that takes in a secret number between 1 and 10. Ask the
+#user to keep guessing the secret number until they guess correctly.
